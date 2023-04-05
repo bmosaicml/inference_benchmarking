23d742340a745e9da11668f084f17889b3cfc0cf
bmosaicml <jeremy@mosaicml.com>
2023-04-04 23:14:37 -0400

    add gpt 3.5 turbo

--- chatgpt.py
+++ chatgpt.py
@@ -14,43 +14,58 @@ openai.api_key = config['OPENAI']
 ITERS = 5
 with open('openai.tsv', 'w') as f:
     f.write(f"model_name\tprompt_len\toutput_len\tcall_latency\tsecs_per_tok\n")
-    for num1 in [1, 16, 32, 64, 128, 256]:
-        for num2 in [1, 16, 32, 64, 128, 256]:
+    for num1 in [ 256]:
+        for num2 in [ 512]:
             prompt = f"Here are the first {num1} even numbers: " + ', '.join([str(2 * i) for i in range(1, num1+1)]) + f"\nGive the first {num2} odd numbers:"
-            for engine in ["text-babbage-001", "text-davinci-003", "text-curie-001",  "davinci-instruct-beta"]:
+            for engine in ["gpt-3.5-turbo", "text-babbage-001", "text-davinci-003", "text-curie-001",  "davinci-instruct-beta"]:
                 print(f"{engine}")
                 avg_latency = 0.0
                 avg_latency_per_token = 0.0
                 avg_prompt_len = 0.0
                 avg_output_len = 0.0
                 for i in range(ITERS):
-                    try:
+                    if engine == "gpt-3.5-turbo":
+                        start = time.time()
+                        response = openai.ChatCompletion.create(
+                            model=engine,
+                            messages=[
+                                {"role": "system", "content": prompt}, 
+                            ],
+                            max_tokens=num2*2,  
+                            temperature=0.25,
+                            top_p=1,
+                            frequency_penalty=0.5,
+                            presence_penalty=0
+                        )
+                        end = time.time()
+                    else:
                         try:
-                            start = time.time()
-                            response = openai.ChatCompletion.create(
-                                engine=engine,
-                                prompt=prompt,
-                                max_tokens=100,  
-                                temperature=0.25,
-                                top_p=1,
-                                frequency_penalty=0.5,
-                                presence_penalty=0
-                            )
-                            end = time.time()
-                        except:
-                            start = time.time()
-                            response = openai.Completion.create(
-                                engine=engine,
-                                prompt=prompt,
-                                max_tokens=100,  
-                                temperature=0.25,
-                                top_p=1,
-                                frequency_penalty=0.5,
-                                presence_penalty=0
-                            )
-                            end = time.time()
-                    except openai.error.RateLimitError:
-                        time.sleep(10)
+                            try:
+                                start = time.time()
+                                response = openai.ChatCompletion.create(
+                                    engine=engine,
+                                    prompt=prompt,
+                                    max_tokens=num2*2,  
+                                    temperature=0.25,
+                                    top_p=1,
+                                    frequency_penalty=0.5,
+                                    presence_penalty=0
+                                )
+                                end = time.time()
+                            except:
+                                start = time.time()
+                                response = openai.Completion.create(
+                                    engine=engine,
+                                    prompt=prompt,
+                                    max_tokens=num2*2,  
+                                    temperature=0.25,
+                                    top_p=1,
+                                    frequency_penalty=0.5,
+                                    presence_penalty=0
+                                )
+                                end = time.time()
+                        except openai.error.RateLimitError:
+                            time.sleep(10)
 
                     prompt_len =  response.usage.prompt_tokens
                     output_len =  response.usage.completion_tokens
